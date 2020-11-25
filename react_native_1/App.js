f8b74fb2e042571875ec2ac536d439050fd6b57a
umssung <em6721@naver.com>
2020-11-25 13:24:05 +0900

    * basic content add - Button - TouchEvent - ScrollView - TextInput

--- react_native_1/App.js
+++ react_native_1/App.js
@@ -10,19 +10,69 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  ScrollView
 } from 'react-native';
 import Header from './src/header'
+import Generator from './src/generator'
+import NumList from './src/numlist'
+import Input from './src/input'
 
 class App extends Component {
 
   state = {
-    appName: 'My First App'
+    appName: 'My First App',
+    random: [36, 999]
+  }
+
+  onAddRandomNum = () => {
+    const randomNum = Math.floor(Math.random()*100)+1
+    this.setState(prevState => {
+      return {
+        random: [...prevState.random, randomNum]
+      }
+    })
+  }
+
+  onDeleteNum = (position) => {
+    const newArray = this.state.random.filter((num, index)=> {
+      return position != index;
+    })
+    this.setState({
+      random: newArray
+    })
   }
   render() {
     return (
       <View style={styles.mainView}>
-        <Header name={this.state.appName}/>
+        {/* <Header name={this.state.appName}/>
+
+        <View>
+          <Text
+            style={styles.mainText}
+            onPress={()=>alert('test touch event')}
+          >
+            Hello World
+          </Text>
+        </View>
+        
+        <Generator add={this.onAddRandomNum} />
+
+        <ScrollView
+          style={{width: '100%'}}
+          // onMomentumScrollBegin={()=>alert('begin')}// is scrolling begin moment
+          // onMomentumScrollEnd={()=>alert('end')} // is scrolling end moment
+          // onScroll={()=>alert('Scrolling')}  //is scroll moment
+          // onContentSizeChange={(width, height)=>alert(height)} //changed content box size
+          bounces={false} //begin or end point bounce
+        >
+          <NumList 
+            num={this.state.random}
+            delete={this.onDeleteNum}
+          />
+        </ScrollView> */}
+
+        <Input />
       </View>
     )
   }
@@ -34,7 +84,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingTop: 50,
     alignItems: 'center',
-    justifyContent: 'center'
+    // justifyContent: 'center'
   },
   subView: {
     backgroundColor: 'yellow',
